7dbbae052875c2076db7a73756ed2a42cecc9f02
aftabmankapure <aftabmankapure@gmail.com>
2023-07-14 13:28:43 +0530

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.create(username:"aman",password:"password")
+# User.create(username:"sahil",password:"password")
+# User.create(username:"ajay",password:"password")
+# User.create(username:"demo",password:"password")
+
+
+Message.create(body: "hi from aman ",user:"aman")
