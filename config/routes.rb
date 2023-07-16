a782ed287ae394036d317982fd28c54fe70ebff3
aftabmankapure <aftabmankapure@gmail.com>
2023-07-16 16:22:04 +0530

    Add sessions create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'chatroom/index'
   root 'chatroom#index'
   get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'delete', to: 'session#destroy'
 end
